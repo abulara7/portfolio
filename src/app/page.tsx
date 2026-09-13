945a481ee6e07c8f140f29675e05db9f4397c7bb
AbuLara <haideralasady123@gmail.com>
2026-09-14 02:17:01 +0300

    feat: remove hackathons section and data completely

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { DATA } from "@/data/resume";
 import Link from "next/link";
 import Markdown from "react-markdown";
 import ContactSection from "@/components/section/contact-section";
-import HackathonsSection from "@/components/section/hackathons-section";
 import ProjectsSection from "@/components/section/projects-section";
 import WorkSection from "@/components/section/work-section";
 import CreativeSection from "@/components/section/creative-section";
@@ -164,12 +163,6 @@ export default function Page() {
           </BlurFade>
         </section>
 
-        {/* Hackathons */}
-        <section id="hackathons">
-          <BlurFade delay={BLUR_FADE_DELAY * 13}>
-            <HackathonsSection />
-          </BlurFade>
-        </section>
       </div>
 
       {/* DIMENSION DIVIDER */}
